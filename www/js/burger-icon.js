584aa812731aa0ac8854b4cb9635e6b75bc08c60
Alex Booker <alexbooker@fastmail.im>
2015-06-11 16:22:57 +0100

    use jQuery consistently
    use jQuery more consistently and added some comments for the burger scss

--- www/js/burger-icon.js
+++ www/js/burger-icon.js
@@ -1,12 +1,12 @@
-(function() {
-  var element = document.querySelector(".burger__icon");
-  element.addEventListener("click", function() {
-    var value = element.getAttribute("data-burger-state");
-    var state = (value === "on") ? "off" : "on";
-    element.setAttribute("data-burger-state", state);
-    element.setCurrentTime(0);
-
-    $(".navigation").animate({opacity: 'toggle', height: 'toggle'}, 200);
-
+$(function() {
+  var burger = $(".burger__icon");
+  burger.click(function() {
+    var state = burger.attr("data-burger-state");
+    burger.attr("data-burger-state", (state === "on") ? "off" : "on");
+    burger[0].setCurrentTime(0);
+    $(".navigation").animate({
+      opacity: 'toggle',
+      height: 'toggle'
+    }, 200);
   });
-}());
+});
