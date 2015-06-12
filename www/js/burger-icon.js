6c1db4fb5e45cd31679cb86671c8b0fcd7c56637
Alex Booker <alexbooker@fastmail.im>
2015-06-12 09:01:31 +0100

    fixed burger icon in firefox

--- www/js/burger-icon.js
+++ www/js/burger-icon.js
@@ -1,9 +1,10 @@
 $(function() {
-  var burger = $(".burger__icon");
+  var burger = $(".burger");
+  var burgerIcon = $(".burger__icon");
   burger.click(function() {
     var state = burger.attr("data-burger-state");
     burger.attr("data-burger-state", (state === "on") ? "off" : "on");
-    burger[0].setCurrentTime(0);
+    burgerIcon[0].setCurrentTime(0);
     $(".navigation").animate({
       opacity: 'toggle',
       height: 'toggle'
